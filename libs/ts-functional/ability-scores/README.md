13232547032a3b0d53853b605917033dedd43c1b
awarrington0895 <awarrington0895@gmail.com>
2021-08-22 18:10:37 +0000

    Add config to allow nx to run library

---
+++ libs/ts-functional/ability-scores/README.md
@@ -0,0 +1,7 @@
+# how-to-jest
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test how-to-jest` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/ts-functional/ability-scores/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'ability-scores',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/ability-scores',
+};
